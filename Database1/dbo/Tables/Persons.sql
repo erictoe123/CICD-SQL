ffe5040feb1389434f890759b13da435b9273a5c
unknown <eric.toe123@gmail.com>
2023-04-13 16:04:47 -0400

    load the solution

---
+++ Database1/dbo/Tables/Persons.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Persons] (
+    [ID]        INT           NOT NULL,
+    [LastName]  VARCHAR (255) NOT NULL,
+    [FirstName] VARCHAR (255) NOT NULL,
+    [Age]       INT           NULL
+);
+
